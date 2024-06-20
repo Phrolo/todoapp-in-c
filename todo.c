61e9db9ba14f7ab32174f4217f10e9e559a9e5f3
Phrolo <cystrafe29@gmail.com>
2024-06-20 14:24:43 +0800

    added printMenu() and main program loop

--- todo.c
+++ todo.c
@@ -17,6 +17,22 @@ typedef struct TASK_STRUCT {
   struct TASK_STRUCT * next;
 } task;
 
+// Print the menu and return the option.
+int printMenu(){
+  int choice = -1;
+
+  printf("[1] Add task.\n");
+  printf("[2] View tasks.\n");
+  printf("[3] Delete tasks.\n");
+  printf("[0] Quit.\n");
+  printf("[o] Choice: ");
+
+  scanf("%d", &choice);
+  getchar();
+
+  return choice;
+}
+
 
  /*
   * Main program.
@@ -26,5 +42,8 @@ int main(int argc, char *argv[])
   int choice = -1;
   task * head = NULL;
 
+  while (choice != 0) {
+    choice = printMenu();
+  }
   return EXIT_SUCCESS;
 }
